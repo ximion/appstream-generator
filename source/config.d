5f85d775ea840915a8f7300c4ed10625f4a05621
Matthias Klumpp <matthias@tenstral.net>
2016-04-18 18:28:22 +0200

    trivial: post release version bump

--- source/config.d
+++ source/config.d
@@ -30,7 +30,7 @@ import ag.utils;
 import ag.logging;
 
 
-public immutable generatorVersion = "0.1";
+public immutable generatorVersion = "0.2";
 
 /**
  * Describes a suite in a software repository.
