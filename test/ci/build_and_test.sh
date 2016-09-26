9f5ef15d0a51e702c494108bd73c4362273aeab7
Matthias Klumpp <matthias@tenstral.net>
2016-09-26 04:58:40 +0200

    ci: Test Meson builds as well

--- test/ci/build_and_test.sh
+++ test/ci/build_and_test.sh
@@ -10,8 +10,21 @@ dub --version
 # on the CI system.
 #
 
+# Build with dub
 dub build --parallel -v
-dub test
 
+# Build with Meson
+mkdir -p build && cd build
+meson ..
+ninja
+
+# Run tests
+./asgen_test
+
+# Test (Meson) install
+DESTDIR=/tmp/install-ninja ninja install
+cd ..
+
+# Test getting JS stuff and installing (dub)
 make js
 make install DESTDIR=/tmp/install-tmp
