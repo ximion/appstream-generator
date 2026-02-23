5eda4b257a3ef7992f2ba5ea793012818e47ae8e
Matthias Klumpp <matthias@tenstral.net>
2026-02-23 20:33:27 +0100

    Double-quote strings in YAML header
    This just brings the style in-line with what libappstream now produces,
    but otherwise changes nothing.

--- src/engine.cpp
+++ src/engine.cpp
@@ -403,7 +403,7 @@ std::string Engine::getMetadataHead(const Suite &suite, const std::string &secti
         head = "%YAML 1.2\n---\n";
         head += std::format(
             "File: DEP-11\n"
-            "Version: '{}'\n"
+            "Version: \"{}\"\n"
             "Origin: {}",
             m_conf->formatVersionStr(),
             origin);
@@ -412,7 +412,7 @@ std::string Engine::getMetadataHead(const Suite &suite, const std::string &secti
         if (suite.dataPriority != 0)
             head += std::format("\nPriority: {}", suite.dataPriority);
         if (m_conf->feature.metadataTimestamps)
-            head += std::format("\nTime: '{}'", timeNowIso8601);
+            head += std::format("\nTime: \"{}\"", timeNowIso8601);
     }
 
     return head;
